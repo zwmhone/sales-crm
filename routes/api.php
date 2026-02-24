35a8794404f8f8d15cb50e2893696785fdb9ad13
Sophia <nahinekrisia@gmail.com>
2026-02-24 08:26:50 +0800

    Dashboard Routes

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CsvImportController;
+use App\Http\Controllers\DashboardController;
 
 Route::post('/csv-import', [CsvImportController::class, 'store']);
+
+
+
+// Dashboard routes
+Route::get('/dashboard/stats', [DashboardController::class, 'stats']);
+Route::get('/dashboard/exceptions', [DashboardController::class, 'exceptions']);
+// Route::post('/follow-up-tasks/{taskId}/action', [DashboardController::class, 'performAction']);
